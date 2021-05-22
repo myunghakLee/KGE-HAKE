98cd2a5cf5b3fa32b754237ea55b7cefb13d65e7
myunghaklee <mh9716@naver.com>
2021-05-22 13:16:57 +0000

    annotation

---
+++ run_FB15-K.sh
@@ -0,0 +1,29 @@
+bash runs.sh train HAKE FB15k-237 0 0 1024 256 1000 9.0 1.0 0.00005 100000 16 3.5 1.0
+mv models/HAKE_wn18rr_0 models/HAKE_wn18rr_0_0
+
+bash runs.sh train HAKE FB15k-237 0 0 1024 256 1000 9.0 1.0 0.00005 100000 16 3.5 1.0
+mv models/HAKE_wn18rr_0 models/HAKE_wn18rr_0_0
+
+bash runs.sh train HAKE FB15k-237 0 0 1024 256 1000 9.0 1.0 0.00005 100000 16 3.5 1.0
+mv models/HAKE_wn18rr_0 models/HAKE_wn18rr_0_0
+
+bash runs.sh train HAKE FB15k-237 0 0 1024 256 1000 9.0 1.0 0.00005 100000 16 3.5 1.0
+mv models/HAKE_wn18rr_0 models/HAKE_wn18rr_0_0
+
+bash runs.sh train HAKE FB15k-237 0 0 1024 256 1000 9.0 1.0 0.00005 100000 16 3.5 1.0
+mv models/HAKE_wn18rr_0 models/HAKE_wn18rr_0_0
+
+bash runs.sh train HAKE FB15k-237 0 0 1024 256 1000 9.0 1.0 0.00005 100000 16 3.5 1.0
+mv models/HAKE_wn18rr_0 models/HAKE_wn18rr_0_0
+
+bash runs.sh train HAKE FB15k-237 0 0 1024 256 1000 9.0 1.0 0.00005 100000 16 3.5 1.0
+mv models/HAKE_wn18rr_0 models/HAKE_wn18rr_0_0
+
+bash runs.sh train HAKE FB15k-237 0 0 1024 256 1000 9.0 1.0 0.00005 100000 16 3.5 1.0
+mv models/HAKE_wn18rr_0 models/HAKE_wn18rr_0_0
+
+bash runs.sh train HAKE FB15k-237 0 0 1024 256 1000 9.0 1.0 0.00005 100000 16 3.5 1.0
+mv models/HAKE_wn18rr_0 models/HAKE_wn18rr_0_0
+
+bash runs.sh train HAKE FB15k-237 0 0 1024 256 1000 9.0 1.0 0.00005 100000 16 3.5 1.0
+mv models/HAKE_wn18rr_0 models/HAKE_wn18rr_0_0
---
+++ run_HAKE.sh
@@ -0,0 +1,29 @@
+bash runs.sh train HAKE wn18rr 0 0 512 1024 500 6.0 0.5 0.00005 80000 8 0.5 0.5
+mv models/HAKE_wn18rr_0 models/HAKE_wn18rr_0_0
+
+bash runs.sh train HAKE wn18rr 0 0 512 1024 500 6.0 0.5 0.00005 80000 8 0.5 0.5
+mv models/HAKE_wn18rr_0 models/HAKE_wn18rr_0_1
+
+bash runs.sh train HAKE wn18rr 0 0 512 1024 500 6.0 0.5 0.00005 80000 8 0.5 0.5
+mv models/HAKE_wn18rr_0 models/HAKE_wn18rr_0_2
+
+bash runs.sh train HAKE wn18rr 0 0 512 1024 500 6.0 0.5 0.00005 80000 8 0.5 0.5
+mv models/HAKE_wn18rr_0 models/HAKE_wn18rr_0_3
+
+bash runs.sh train HAKE wn18rr 0 0 512 1024 500 6.0 0.5 0.00005 80000 8 0.5 0.5
+mv models/HAKE_wn18rr_0 models/HAKE_wn18rr_0_4
+
+bash runs.sh train HAKE wn18rr 0 0 512 1024 500 6.0 0.5 0.00005 80000 8 0.5 0.5
+mv models/HAKE_wn18rr_0 models/HAKE_wn18rr_0_5
+
+bash runs.sh train HAKE wn18rr 0 0 512 1024 500 6.0 0.5 0.00005 80000 8 0.5 0.5
+mv models/HAKE_wn18rr_0 models/HAKE_wn18rr_0_6
+
+bash runs.sh train HAKE wn18rr 0 0 512 1024 500 6.0 0.5 0.00005 80000 8 0.5 0.5
+mv models/HAKE_wn18rr_0 models/HAKE_wn18rr_0_7
+
+bash runs.sh train HAKE wn18rr 0 0 512 1024 500 6.0 0.5 0.00005 80000 8 0.5 0.5
+mv models/HAKE_wn18rr_0 models/HAKE_wn18rr_0_8
+
+bash runs.sh train HAKE wn18rr 0 0 512 1024 500 6.0 0.5 0.00005 80000 8 0.5 0.5
+mv models/HAKE_wn18rr_0 models/HAKE_wn18rr_0_9
